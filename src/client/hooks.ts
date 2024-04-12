c747ef66b6127bd08ea670aaf2438f780fbbd9bf
Aurelien Brabant <aurelien@brabant.dev>
2024-04-12 21:23:19 +0200

    feat: add login

--- src/client/hooks.ts
+++ src/client/hooks.ts
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { authContext } from "./provider"
 
 export const useAuth = () => {
-	const { user } = useContext(authContext);
+	const { user, logout, login } = useContext(authContext);
 
-	return { user };
+	return { user, logout, login };
 }
--- src/client/provider.tsx
+++ src/client/provider.tsx
@@ -8,11 +8,13 @@ import { useRouter } from 'next/navigation';
 export type AuthContextValue = {
 	user: User | null;
 	logout: () => void;
+	login: () => void;
 }
 
 export const authContext = createContext<AuthContextValue>({
 	user: null,
 	logout: () => {},
+	login: () => {}
 })
 
 export const ClientProvider: FunctionComponent<{ value: { user: User | null }, children: ReactNode }> = ({ value, children }) => {
@@ -22,8 +24,13 @@ export const ClientProvider: FunctionComponent<{ value: { user: User | null }, c
 		router.replace('/logout');
 	}
 
+	const login = () => {
+		router.replace('/login');
+	}
+
 	return <authContext.Provider value={{
 		user: value.user,
+		login,
 		logout
 	}}>{children}</authContext.Provider>;
 }
