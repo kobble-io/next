8caed1bcfda2d0d1e99a3c4979fb754884e08eef
Francois Duval <francois.duval.auto@gmail.com>
2024-06-23 00:37:52 +0200

    Secures domain url format as url or domain

--- src/server/config.ts
+++ src/server/config.ts
@@ -12,10 +12,21 @@ const loadConfig = (): Config => {
 		throw new Error('Missing NEXT_PUBLIC_KOBBLE_CLIENT_ID');
 	}
 
-	const portalUrl = process.env.NEXT_PUBLIC_KOBBLE_DOMAIN;
+	let portalUrl = process.env.NEXT_PUBLIC_KOBBLE_DOMAIN;
 
 	if (!portalUrl) {
 		throw new Error('Missing NEXT_PUBLIC_KOBBLE_DOMAIN');
+	}
+
+	try {
+		portalUrl = new URL(portalUrl).href;
+	} catch (error) {
+		try {
+			portalUrl = new URL(`https://${portalUrl}`).href;
+			console.warn('NEXT_PUBLIC_KOBBLE_DOMAIN is used as a domain but should start with https://');
+		} catch (error) {
+			throw new Error('Invalid format NEXT_PUBLIC_KOBBLE_DOMAIN');
+		}
 	}
 
 	const redirectUri = process.env.NEXT_PUBLIC_KOBBLE_REDIRECT_URI;
